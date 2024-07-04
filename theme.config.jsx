775f5f82fdbcae2febaf0a5d792456232930f6c1
0xSage <n@pragmatic.vc>
2024-07-04 12:28:47 +0800

    add pages

---
+++ theme.config.jsx
@@ -0,0 +1,8 @@
+export default {
+    logo: <span>☕️ Homebrew Computer Company</span>,
+    project: {
+        link: 'https://github.com/janhq'
+    },
+    docsRepositoryBase: "https://github.com/janhq/homebrew"
+    // ... other theme options
+}
