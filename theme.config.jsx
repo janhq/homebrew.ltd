52d05930d852d74284d221ed4acc39064a60b847
0xSage <n@pragmatic.vc>
2024-07-04 14:57:26 +0800

    feat: blog section in homepage

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,8 +1,39 @@
 export default {
-    logo: <span>☕️ Homebrew Computer Company</span>,
+    logo: <span>☕️ Homebrew</span>,
     project: {
         link: 'https://github.com/janhq'
     },
-    docsRepositoryBase: "https://github.com/janhq/homebrew"
+    chat: {
+        link: 'https://discord.gg/VSbRN3vwCD',
+    },
+    docsRepositoryBase: "https://github.com/janhq/homebrew/tree/main",
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s – Homebrew'
+        }
+    },
+    head: (
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta property="og:title" content="Homebrew" />
+            <meta property="og:description" content="Open Source, AI R&D Lab" />
+        </>
+    ),
+    navigation: {
+        prev: true,
+        next: true
+    },
+    footer: {
+        text: (
+            <span>
+                Apache 2.0 {new Date().getFullYear()} ©{' '}
+                <a href="https://nextra.site" target="_blank">
+                    Homebrew Computer Company Pte Ltd.
+                </a>
+                .
+            </span>
+        )
+    },
+    faviconGlyph: "☕️"
     // ... other theme options
 }
