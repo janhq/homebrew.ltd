eee62fd06ff10c469531b05b5df8b0a642ad3c5d
Faisal Amir <urmauur@gmail.com>
2024-07-16 10:15:00 +0700

    feat: simplify landing page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,32 +1,35 @@
-import { useData } from 'nextra/data'
-import { format } from 'date-fns'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useData } from "nextra/data";
+import { format } from "date-fns";
+import { useRouter, useSearchParams } from "next/navigation";
 
-import Link from 'next/link'
-import { Cards } from 'nextra/components'
-import { twMerge } from 'tailwind-merge'
+import Link from "next/link";
+import { Cards } from "nextra/components";
+import { twMerge } from "tailwind-merge";
 
 const Home = () => {
-    const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
 
-    return (
-        <div className="nextra-wrap-container py-14">
-            <div className="w-full mx-auto">
-                <h1 className="text-6xl !fqont-normal leading-tight lg:leading-tight mt-2 font-serif">
-                    Homebrew Computer Company
-                </h1>
-                <div className="text-black/60 dark:text-white/60">
-                    <p className="text-base mt-2 leading-relaxed">
-                        Homebrew is an R&D lab focused on on-device and privacy centric AI solutions.
-                        <br />
-                        We solve problems across the stack, from model training, to tooling, to hardware design.
-                        <br />
-                        We are open-source and build-in-public. You can find our ongoing research insights below.
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="nextra-wrap-container py-14">
+      <div className="w-full mx-auto">
+        <h1 className="text-6xl !fqont-normal leading-tight lg:leading-tight mt-2 font-serif">
+          Homebrew Computer Company
+        </h1>
+        <div className="text-black/60 dark:text-white/60">
+          <p className="text-base mt-2 leading-relaxed">
+            Homebrew is an R&D lab focused on on-device and privacy centric AI
+            solutions.
+            <br />
+            We solve problems across the stack, from model training, to tooling,
+            to hardware design.
+            <br />
+            We are open-source and build-in-public. You can find our ongoing
+            research insights below.
+          </p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
