b6bc981aea8cd763f0615806ea813b7cb2827f32
Faisal Amir <urmauur@gmail.com>
2024-07-16 13:51:21 +0700

    chore: setup font and reduce contianer width

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -3,8 +3,6 @@ import { format } from "date-fns";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Link from "next/link";
-import { Cards } from "nextra/components";
-import { twMerge } from "tailwind-merge";
 
 const Blog = () => {
   const blogPost = useData();
@@ -12,111 +10,40 @@ const Blog = () => {
   const search = searchParams?.get("category");
   const router = useRouter();
 
-  const staticCategories = [
-    {
-      name: "Engineering",
-      id: "engineering",
-    },
-    {
-      name: "Research",
-      id: "research",
-    },
-  ];
-
   return (
-    <div className="nextra-wrap-container py-14">
+    <div className="py-14">
+      <h2 className="nx-mt-2 nx-text-3xl nx-font-bold nx-tracking-tight nx-text-slate-900 dark:nx-text-slate-100">
+        Blog
+      </h2>
       <div className="w-full mx-auto">
-        <div className="mt-10">
-          <ul className="flex lg:gap-4 gap-1 whitespace-nowrap overflow-auto lg:overflow-hidden lg:whitespace-normal">
-            <li
-              onClick={() => {
-                router.push(`/`);
-              }}
-              className={twMerge(
-                "cursor-pointer py-1 px-2 lg:px-3 rounded-full",
-                search === null &&
-                  "dark:bg-blue-400 bg-blue-500 font-medium text-white"
-              )}
-            >
-              <p>All Categories</p>
-            </li>
-            {staticCategories.map((cat, i) => {
-              return (
-                <li
-                  key={i}
-                  onClick={() => {
-                    router.push(`/?category=${cat.id}`);
-                  }}
-                  className={twMerge(
-                    "cursor-pointer py-1 px-2 lg:px-3 rounded-full",
-                    cat.id === search &&
-                      "dark:bg-blue-400 bg-blue-500 font-medium text-white"
-                  )}
-                >
-                  <p>{cat.name}</p>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-
-        <Cards num={4} className="mt-14 gap-8">
-          {blogPost
-            .filter((post: BlogPostsThumbnail) => {
-              if (search) {
-                return post.categories?.includes(String(search));
-              } else {
-                return post;
-              }
-            })
-            .map((post: BlogPostsThumbnail, i: number) => {
-              return (
-                <Link
-                  href={String(post.url)}
-                  key={i}
-                  className="nextra-card nx-group nx-flex nx-flex-col nx-justify-start nx-overflow-hidden nx-rounded-xl nx-border nx-border-gray-200 nx-text-current nx-no-underline dark:nx-shadow-none hover:nx-shadow-gray-100 dark:hover:nx-shadow-none nx-shadow-gray-100 active:nx-shadow-sm active:nx-shadow-gray-200 nx-transition-all nx-duration-200 hover:nx-border-gray-300 nx-bg-transparent nx-shadow-sm dark:nx-border-neutral-800 hover:nx-shadow-md dark:hover:nx-border-neutral-700"
-                >
-                  <div
-                    className={twMerge(
-                      "min-h-40 border-b border-gray-200 dark:border-neutral-800",
-                      i % 2 !== 0
-                        ? "bg-gradient-to-r from-cyan-500 to-blue-500"
-                        : "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
-                    )}
-                  >
-                    <div className="flex w-full h-full items-center px-4 justify-center">
-                      <div className="text-center">
-                        {post.categories?.map((cat, i) => {
-                          return (
-                            <p
-                              className="inline-flex capitalize text-xl font-bold text-white"
-                              key={i}
-                            >
-                              {cat?.replaceAll("-", " ")}
-                            </p>
-                          );
-                        })}
-                        <p className="font-medium text-white">
-                          {format(String(post.date), "MMMM do, yyyy")}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="px-4 py-6">
-                    <h6 className="text-lg line-clamp-1 font-bold">
-                      {post.title}
-                    </h6>
-                    <p className="my-2 text-black/60 dark:text-white/60 line-clamp-2 leading-relaxed">
-                      {post.description}
-                    </p>
-                    <p className="dark:text-blue-400 text-blue-600 line-clamp-2 font-medium">
-                      Read more...
-                    </p>
-                  </div>
-                </Link>
-              );
-            })}
-        </Cards>
+        {blogPost
+          .filter((post: BlogPostsThumbnail) => {
+            if (search) {
+              return post.categories?.includes(String(search));
+            } else {
+              return post;
+            }
+          })
+          .map((post: BlogPostsThumbnail, i: number) => {
+            return (
+              <Link href={String(post.url)} key={i}>
+                <div className="py-6">
+                  <h6 className="text-lg line-clamp-1 font-medium font-sans">
+                    {post.title}
+                  </h6>
+                  <p className="my-2 text-black/70 dark:text-white/70 line-clamp-2 leading-relaxed text-sm">
+                    {post.description}
+                  </p>
+                  <p className="font-medium text-black/70 dark:text-white/70 text-sm">
+                    {format(String(post.date), "MMMM do, yyyy")}
+                  </p>
+                  <p className="dark:text-blue-400 text-blue-600 line-clamp-2 font-medium mt-4 text-sm">
+                    Read more...
+                  </p>
+                </div>
+              </Link>
+            );
+          })}
       </div>
     </div>
   );
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,42 +1,60 @@
-import FooterMenu from '@/components/FooterMenu'
-import Logo from './public/logo-homebrew.svg'
-import Image from 'next/image'
-
+import FooterMenu from "@/components/FooterMenu";
+import Logo from "./public/logo-homebrew.svg";
+import Image from "next/image";
 
 export default {
-    logo: <>
-    <Image
-    width={40}
-    height={40}
-    style={{marginRight: "10px"}}
-    src={Logo}
-    alt="Homebrew - Logo"
-  /><span className="logo-mobile" style={{fontWeight: "bold"}}> Homebrew </span></>,
-    project: {
-        link: 'https://github.com/janhq'
-    },
-    chat: {
-        link: 'https://discord.gg/VSbRN3vwCD',
-    },
-    docsRepositoryBase: "https://github.com/janhq/homebrew/tree/main",
-    useNextSeoProps() {
-        return {
-            titleTemplate: '%s – Homebrew'
-        }
-    },
-    head: (
-        <>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta property="og:title" content="Homebrew" />
-            <meta property="og:description" content="Open Source, AI R&D Lab" />
-            <link rel="icon" href="/logo-homebrew.svg" type="image/svg" />
-        </>
+  logo: (
+    <>
+      <Image
+        width={40}
+        height={40}
+        style={{ marginRight: "10px" }}
+        src={Logo}
+        alt="Homebrew - Logo"
+      />
+      <span className="logo-mobile" style={{ fontWeight: "bold" }}>
+        {" "}
+        Homebrew{" "}
+      </span>
+    </>
+  ),
+  project: {
+    link: "https://github.com/janhq",
+  },
+  chat: {
+    link: "https://discord.gg/VSbRN3vwCD",
+  },
+  docsRepositoryBase: "https://github.com/janhq/homebrew/tree/main",
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Homebrew",
+    };
+  },
+  toc: {
+    component: <></>,
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Homebrew" />
+      <meta property="og:description" content="Open Source, AI R&D Lab" />
+      <link rel="icon" href="/logo-homebrew.svg" type="image/svg" />
+    </>
+  ),
+  navigation: {
+    prev: true,
+    next: true,
+  },
+  footer: {
+    // text: <FooterMenu />,
+    text: (
+      <span>
+        ©{new Date().getFullYear()}{" "}
+        <a href="https://nextra.site" target="_blank">
+          Homebrew Computer Company
+        </a>
+        .
+      </span>
     ),
-    navigation: {
-        prev: true,
-        next: true
-    },
-    footer: {
-        text: <FooterMenu />,
-    }
-}
+  },
+};
