f140f40c497fdd5121a0b2c7a821cf66a699358f
Faisal Amir <urmauur@gmail.com>
2024-07-30 13:53:36 +0700

    feat: setup gtm analytic (#44)

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
-const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx'
-})
-
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
 
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-    images: {
-        formats: ['image/webp'],
-        unoptimized: true,
-    },
-}
+  reactStrictMode: true,
+  output: "export",
+  env: {
+    GTM_ID: process.env.GTM_ID,
+  },
+  images: {
+    formats: ["image/webp"],
+    unoptimized: true,
+  },
+};
 
-module.exports = withNextra(nextConfig)
+module.exports = withNextra(nextConfig);
