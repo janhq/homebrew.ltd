a9f728180466aaa80b4067dc8972f4a72526b6ef
Faisal Amir <urmauur@gmail.com>
2024-09-10 13:22:13 +0700

    chore: make banner blog full width and use neutral tone color

--- src/components/Blog/Banner.tsx
+++ src/components/Blog/Banner.tsx
@@ -14,7 +14,7 @@ const BlogBanner = () => {
 
   return (
     <Link href={String(latestPost.url)} target="_blank" className="block">
-      <div className="inline-flex my-2 py-2 pl-2 pr-4 rounded-lg mb-8 bg-indigo-500 text-white">
+      <div className="inline-flex w-full my-2 py-2 pl-2 pr-4 rounded-lg mb-8 bg-neutral-50 dark:bg-neutral-700 border border-neutral-100 dark:border-neutral-600 text-neutral-700 dark:text-neutral-200">
         <div className="flex items-center rounded bg-white px-2">
           <span className="font-bold text-sm uppercase text-blue-600">new</span>
         </div>
